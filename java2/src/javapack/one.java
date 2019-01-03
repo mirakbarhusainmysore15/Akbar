8f63ae5213b27c526436eb6c9d61ceaaa73226c1
mirakbarhusainmysore15 <46221950+mirakbarhusainmysore15@users.noreply.github.com>
2019-01-03 21:40:08 +0530

    Update one.java

--- java2/src/javapack/one.java
+++ java2/src/javapack/one.java
@@ -5,7 +5,7 @@ public class one
 
 	public static void main(String[] args)
 	{
- System.out.println("welcome to github and jenkins");
+ System.out.println("welcome to github and jenkins Jeelu");
 	}
 
 }
